229125330b473e96d44ade2deb926d058418c7b4
Hashem Erainat <h.erainat1@student.aaup.edu>
2024-03-25 01:06:42 +0200

    insert install Splash Screen

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -1,14 +1,45 @@
-package com.example.e_commerce
 
-import androidx.appcompat.app.AppCompatActivity
+package com.training.ecommerce
+
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.e_commerce.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        initSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
     }
+
+    private fun initSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+            // Add a callback that's called when the splash screen is animating to the
+            // app content.
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView, View.TRANSLATION_Y, 0f, -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 1000L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
+        } else {
+            setTheme(R.style.Theme_ECommerce)
+        }
+    }
+
 }
