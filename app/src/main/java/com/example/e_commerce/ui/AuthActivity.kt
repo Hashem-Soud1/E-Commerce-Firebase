07b099a6a5e76dd35af11bf0b6f5f8eda1d5aa2a
Hashem Erainat <h.erainat1@student.aaup.edu>
2024-03-26 14:39:29 +0200

    add data and view binding  and nav component

---
+++ app/src/main/java/com/example/e_commerce/ui/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_commerce.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
