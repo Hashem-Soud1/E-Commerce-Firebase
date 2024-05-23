54af4f3b823912bd8da7f2abb4c778b1a116c782
Hashem Erainat <h.erainat1@student.aaup.edu>
2024-05-23 18:34:13 +0300

    fix loginFragment.kt

--- app/src/main/java/com/example/e_commerce/ui/auth/fragment/loginFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/auth/fragment/loginFragment.kt
@@ -2,6 +2,7 @@ package com.example.e_commerce.ui.auth.fragment
 
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,16 +12,29 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import com.example.e_commerce.R
 import com.example.e_commerce.BuildConfig.clientServerId
+import com.example.e_commerce.data.datasource.datastore.AppPreferencesDataSource
 import com.example.e_commerce.data.models.Resource
+import com.example.e_commerce.data.models.user.UserDetailsModel
+import com.example.e_commerce.data.models.user.toUserDetailsPreferences
+import com.example.e_commerce.data.repository.auth.FirebaseAuthRepository
+import com.example.e_commerce.data.repository.auth.FirebaseAuthRepositoryImpl
+import com.example.e_commerce.data.repository.common.AppDataStoreRepositoryImpl
+import com.example.e_commerce.data.repository.common.AppPreferenceRepository
+import com.example.e_commerce.data.repository.user.UserPreferenceRepository
+import com.example.e_commerce.data.repository.user.UserPreferenceRepositoryImpl
 
 import com.example.e_commerce.databinding.FragmentLoginBinding
 import com.example.e_commerce.ui.auth.viewmodel.LoginViewModel
 import com.example.e_commerce.ui.auth.viewmodel.LoginViewModelFactory
 import com.example.e_commerce.ui.common.model.ProgressDialog
 import com.example.e_commerce.ui.home.MainActivity
+import com.example.e_commerce.utils.isValidEmail
 import com.example.e_commerce.utils.showSnakeBarError
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
@@ -33,182 +47,97 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 
-class LoginFragment : Fragment() {
+class LoginViewModel(
+    private val appPreferenceRepository: AppPreferenceRepository,
+    private val userPreferenceRepository: UserPreferenceRepository,
+    private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
 
+    private val _loginState = MutableSharedFlow<Resource<UserDetailsModel>>()
+    val loginState: SharedFlow<Resource<UserDetailsModel>> = _loginState.asSharedFlow()
 
+    val email = MutableStateFlow("")
+    val password = MutableStateFlow("")
 
-    private val loginViewModel: LoginViewModel by viewModels {
-        LoginViewModelFactory(contextValue = requireContext())
+    private val isLoginIsValid: Flow<Boolean> = combine(email, password) { email, password ->
+        email.isValidEmail() && password.length >= 6
     }
 
-    private var _binding: FragmentLoginBinding? = null
-    private  val binding get() = _binding!!
-
-    private val callbackManager: CallbackManager by lazy {
-        CallbackManager.Factory.create()
-    }
-
-    private val loginManager: LoginManager    by lazy {
-        LoginManager.getInstance()
-    }
-
-    private val progressDialog by lazy {
-        ProgressDialog.createProgressDialog(requireActivity())
-    }
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        _binding =FragmentLoginBinding.inflate(inflater, container, false)
-        binding.viewmodel=loginViewModel
-        binding.lifecycleOwner=viewLifecycleOwner
-        return _binding?.root
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding=null
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initListener()
-        initViewModel()
-    }
-
-
-    private fun initListener() {
-        binding.loginBtn.setOnClickListener {
-            loginViewModel.loginWithEmailAndPassword()
-        }
-        binding.googleSigninBtn.setOnClickListener {
-            loginWithGoogleRequest()
-        }
-        binding.facebookSigninBtn.setOnClickListener {
-            loginWithFacebookRequest()
+    fun login() = viewModelScope.launch {
+        val email = email.value
+        val password = password.value
+        if (isLoginIsValid.first()) {
+            handleLoginFlow { authRepository.loginWithEmailAndPassword(email, password) }
+        } else {
+            _loginState.emit(Resource.Error(Exception("Invalid email or password")))
         }
     }
 
-    private fun initViewModel() {
+    suspend fun loginWithGoogle(idToken: String) {
+        handleLoginFlow { authRepository.loginWithGoogle(idToken) }
+    }
 
-        lifecycleScope.launch{
-            loginViewModel.loginState.collect{ resource ->
-                when(resource)
-                {
-                    is Resource.Loading ->   progressDialog.show()
-                    is Resource.Success ->  {
-                        progressDialog.dismiss()
-                        goToHome()
+    suspend fun loginWithFacebook(token: String) {
+        handleLoginFlow { authRepository.loginWithFacebook(token) }
+    }
+
+    private fun handleLoginFlow(loginFlow: suspend () -> Flow<Resource<UserDetailsModel>>) =
+        viewModelScope.launch {
+            loginFlow().onEach { resource ->
+                when (resource) {
+                    is Resource.Success -> {
+                        savePreferenceData(resource.data!!)
+                        _loginState.emit(Resource.Success(resource.data))
                     }
-                    is Resource.Error   -> progressDialog.dismiss()
 
-
+                    else -> _loginState.emit(resource)
                 }
-            }
-        }
-    }
-
-    private fun goToHome() {
-        requireActivity().startActivity(Intent(activity, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        })
-        requireActivity().finish()
-    }
-
-
-    private fun loginWithGoogleRequest() {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(clientServerId)
-            .requestEmail()
-            .requestProfile()
-            .requestServerAuthCode(clientServerId).build()
-
-        val googleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
-        googleSignInClient.signOut()
-        val signInIntent = googleSignInClient.signInIntent
-        launcher.launch(signInIntent)
-    }
-
-    private val launcher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == AppCompatActivity.RESULT_OK) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleSignInWithGooglResult(task)
-            } else {
-                view?.showSnakeBarError(getString(R.string.google_sign_in_field_msg))
-            }
+            }.launchIn(viewModelScope)
         }
 
 
+    private suspend fun savePreferenceData(userDetailsModel: UserDetailsModel) {
+        appPreferenceRepository.saveLoginState(true)
+        userPreferenceRepository.updateUserDetails(userDetailsModel.toUserDetailsPreferences())
+    }
 
-    private fun handleSignInWithGooglResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            firebaseAuthWithGoogle(account.idToken!!)
-        } catch (e: Exception) {
-            view?.showSnakeBarError(e.message ?: getString(R.string.generic_err_msg))
-            val msg = e.message ?: getString(R.string.generic_err_msg)
-            //logAuthIssueToCrashlytics(msg, "Google")
+    companion object {
+        private const val TAG = "LoginViewModel"
+    }
+}
+
+// create viewmodel factory class
+class LoginViewModelFactory(
+    private val contextValue: Context
+) : ViewModelProvider.Factory {
+
+    private val appPreferenceRepository =
+        AppDataStoreRepositoryImpl(AppPreferencesDataSource(contextValue))
+    private val userPreferenceRepository = UserPreferenceRepositoryImpl(contextValue)
+    private val authRepository = FirebaseAuthRepositoryImpl()
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST") return LoginViewModel(
+                appPreferenceRepository,
+                userPreferenceRepository,
+                authRepository,
+            ) as T
         }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        loginViewModel.loginWithGoogle(idToken)
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        callbackManager.onActivityResult(requestCode, resultCode, data)
-    }
-
-    private fun loginWithFacebookRequest() {
-
-      signOut()
-
-        loginManager.registerCallback(callbackManager,object :FacebookCallback<LoginResult>{
-
-          override fun onSuccess(result: LoginResult) {
-
-                val idToken = result.accessToken.token
-                firebaseAuthWithFacebook(idToken)
-            }
-            override fun onCancel() {}
-
-            override fun onError(error: FacebookException) {
-                view?.showSnakeBarError(R.string.facebook_sign_in_field_msg.toString())
-            }
-        })
-
-        loginManager?.logInWithReadPermissions(
-            this, callbackManager , listOf("email", "public_profile")
-        )
-
-
-    }
-
-
-
-    private fun signOut() {
-        loginManager.logOut()
-
-    }
-
-//    private fun isLoggedIn(): Boolean {
-//        val accessToken = AccessToken.getCurrentAccessToken()
-//        return accessToken != null && !accessToken.isExpired
-//    }
-
-    private fun firebaseAuthWithFacebook(idToken: String) {
-        loginViewModel.loginWithFacebook(idToken)
-    }
-
-
-
 }
 
 
