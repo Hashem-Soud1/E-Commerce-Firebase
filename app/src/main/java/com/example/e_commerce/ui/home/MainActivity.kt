a7104c0fdb38fabe3789cef172b3e042aac765c5
Hashem Erainat <h.erainat1@student.aaup.edu>
2024-05-23 11:35:57 +0300

    remove splash animation

--- app/src/main/java/com/example/e_commerce/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/home/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnticipateInterpolator
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
@@ -24,6 +25,7 @@ import com.example.e_commerce.ui.auth.AuthActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,16 +36,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         initSplashScreen()
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch(Dispatchers.Main) {
-            val isLoggedIn = userViewModel.isUserLoggedIn().first()
 
-            if (isLoggedIn) {
-                setContentView(R.layout.activity_main)
-            } else {
+            val isLoggedIn = runBlocking { userViewModel.isUserLoggedIn().first()}
+
+            if (!isLoggedIn)
+            {
                 goToAuthActivity()
+
+          } else {
+                setContentView(R.layout.activity_main)
+                findViewById<TextView>(R.id.maintext).setOnClickListener {
+                    logOut()
+                }
             }
-        }
+
     }
+
 
     override fun onResume() {
         super.onResume()
@@ -59,24 +67,17 @@ class MainActivity : AppCompatActivity() {
         )
         startActivity(intent, options.toBundle())
     }
+    private fun logOut() {
+        lifecycleScope.launch {
+            userViewModel.logout()
+            goToAuthActivity()
+        }
+    }
 
     private fun initSplashScreen() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             installSplashScreen()
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                // Create your custom animation.
-                val slideUp = ObjectAnimator.ofFloat(
-                    splashScreenView, View.TRANSLATION_Y, 0f, -splashScreenView.height.toFloat()
-                )
-                slideUp.interpolator = AnticipateInterpolator()
-                slideUp.duration = 1000L
 
-                // Call SplashScreenView.remove at the end of your custom animation.
-                slideUp.doOnEnd { splashScreenView.remove() }
-
-                // Run your animation.
-                slideUp.start()
-            }
         } else {
             setTheme(R.style.Theme_ECommerce)
         }
