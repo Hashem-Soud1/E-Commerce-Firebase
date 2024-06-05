c84934506c9cb475c0d46c1e53fc191439f6d94c
Hashem Erainat <h.erainat1@student.aaup.edu>
2024-06-05 18:49:35 +0300

    29/4 session(16)

--- app/src/main/java/com/example/e_commerce/data/models/user/UserDetailsModel.kt
+++ app/src/main/java/com/example/e_commerce/data/models/user/UserDetailsModel.kt
@@ -4,15 +4,18 @@ import android.os.Parcelable
 import androidx.annotation.Keep
 import com.google.firebase.firestore.PropertyName
 import kotlinx.parcelize.Parcelize
+import java.util.Date
 
 @Keep
 @Parcelize
 data class UserDetailsModel(
-    @get:PropertyName("created-at")
+  @get:PropertyName("created-at")
   @set:PropertyName("created-at")
-    var createdAt: Long? = null,
-    var id: String? = null,
-    var email: String? = null,
-    var name: String? = null,
-    var reviews: List<String>? = null,
+    var createdAt: Date? = null,
+  var id: String? = null,
+  var email: String? = null,
+  var name: String? = null,
+  var reviews: List<String>? = null,
+  var idToken: String? = null
+
 ) : Parcelable
