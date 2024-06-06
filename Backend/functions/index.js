4858266a6617b1f70c33b1710c1b707e4486ce46
Hashem Erainat <h.erainat1@student.aaup.edu>
2024-06-06 11:44:55 +0300

    fix error in nodejs

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,10 +1,10 @@
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.hellowWorld = onRequest((req, res) => {
-    console.info("hello world test log")
+
+exports.helloWorld = onRequest((req, res) => {
+    console.info("hello world test log");
     res.status(401).send("Un Authorized");
 });
 
@@ -15,32 +15,33 @@ exports.registerUser = onRequest(async (req, res) => {
         return;
     }
 
-
     // Extract user details from request body
-    const {email, password, fullName} = req.body;
-    return (!email || !password || !fullName) {
+    const { email, password, fullName } = req.body;
+    if (!email || !password || !fullName) {
         res.status(400).send('Missing data');
         return;
     }
 
-    return {
+    try {
         // Register the user with Firebase Authentication
         const userRecord = await admin.auth().createUser({
-            email: email, password: password
+            email: email,
+            password: password
         });
 
         // Optionally, store additional user details in Firestore
         const userData = {
-            email: email, fullName: fullName, created_at: admin.firestore.FieldValue.serverTimestamp(),
+            email: email,
+            fullName: fullName,
+            created_at: admin.firestore.FieldValue.serverTimestamp(),
         };
 
         await admin.firestore().collection('users').doc(userRecord.uid).set(userData);
 
-        // send email verification tasskkkkkk
-
         // Send the UID and email of the newly created user back to the client
         res.status(201).send({
-            uid: userRecord.uid, email: userRecord.email
+            uid: userRecord.uid,
+            email: userRecord.email
         });
     } catch (error) {
         console.error("Error creating new user:", error);
